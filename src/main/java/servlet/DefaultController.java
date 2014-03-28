a359aaed533156f444254e9945e846dff1f21165
sjain0@walmartlabs.com <siddhanth.iitb@gmail.com>
2014-03-28 19:44:28 +0530

    Adding basic cordrops code

--- src/main/java/servlet/DefaultController.java
+++ src/main/java/servlet/DefaultController.java
@@ -17,45 +17,44 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.web.servlet.ModelAndView;
 
 @SuppressWarnings("serial")
-@WebServlet(
-        name = "Servlet", 
-        urlPatterns = {"/home"}
-    )
+@WebServlet(name = "Servlet", urlPatterns = { "/home" })
 public class DefaultController extends HttpServlet {
-    protected final Log logger = LogFactory.getLog(getClass());
- 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String now = (new Date()).toString();
-        HttpSession session = request.getSession();
-        ServletContext sc = session.getServletContext();
-        String x = sc.getRealPath("/");
-        String host = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort(); 
-        
-        //ServletOutputStream out = response.getOutputStream();
-        //out.write(host.getBytes());
-        //out.flush();
-        
-        request.setAttribute("path", host);
-        request.setAttribute("now", now);
-        request.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(request, response);
-    }
-    
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-    	ServletOutputStream out = resp.getOutputStream();
-        out.write("heroku".getBytes());
-        out.flush();
-    	handleRequest(req, resp);
-    }
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+	protected final Log logger = LogFactory.getLog(getClass());
 
-    	String now = (new Date()).toString();
-        logger.info("Returning hello view with " + now);
-        return new ModelAndView("/WEB_INF/jsp/hello.jsp", "now", now);
-    }
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		String now = (new Date()).toString();
+		HttpSession session = request.getSession();
+		ServletContext sc = session.getServletContext();
+		String x = sc.getRealPath("/");
+		String host = request.getScheme() + "://" + request.getServerName()
+				+ ":" + request.getServerPort();
+
+		// ServletOutputStream out = response.getOutputStream();
+		// out.write(host.getBytes());
+		// out.flush();
+
+		request.setAttribute("path", host);
+		request.setAttribute("now", now);
+		request.getRequestDispatcher("/WEB-INF/jsp/main.jsp").forward(request,
+				response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		ServletOutputStream out = resp.getOutputStream();
+		out.write("heroku".getBytes());
+		out.flush();
+		handleRequest(req, resp);
+	}
+
+	public ModelAndView handleRequest(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		String now = (new Date()).toString();
+		logger.info("Returning hello view with " + now);
+		return new ModelAndView("/WEB_INF/jsp/hello.jsp", "now", now);
+	}
 }
-
